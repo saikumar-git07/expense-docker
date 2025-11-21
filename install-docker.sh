62f4a421561643b2f7a1f841b6a3e340f77bb180
saikumar <wwwsaikumar150@gmail.com>
2025-11-21 19:29:14 +0530

    docker

--- install-docker.sh
+++ install-docker.sh
@@ -7,10 +7,10 @@ dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl enable --now docker
 usermod -aG docker ec2-user
-echo -e "$R Logout and Login again $N"
 lsblk
 growpart /dev/nvme0n1 4
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
+echo -e "$R Logout and Login again $N"
